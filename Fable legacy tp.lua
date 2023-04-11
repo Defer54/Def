56eb3371e2d4f1419f77d395e1045e85189bf46c
Defer54 <130475191+Defer54@users.noreply.github.com>
2023-04-11 17:53:56 +0400

    Create Fable legacy tp.lua

---
+++ Fable legacy tp.lua
@@ -0,0 +1,123 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Name", "RJTheme3")
+ 
+local Tab = Window:NewTab("TP")
+
+local Section = Tab:NewSection("Section Name")
+ 
+ Section:NewButton("Teleport to 1", "ButtonInfo", function()
+ shared.teleport = true
+local TPCFrame = CFrame.new(88.379715, 4.99999857, 75.6471329, -0.968879104, -6.61273916e-08, 0.247534335, -4.99730071e-08, 1, 7.15439725e-08, -0.247534335, 5.69474246e-08, -0.968879104)--Put a new cframe or cframe property in here
+local User = game.Players.LocalPlayer.Character.HumanoidRootPart
+local WaitTime = 105 --Make More/Less depending if it works or not
+while shared.teleport do wait()
+spawn( function()
+wait(WaitTime)
+shared.teleport = false
+end)
+User.CFrame = TPCFrame
+local args = {
+    [1] = "__--r",
+    [2] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position,
+    [3] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0)
+}
+game:GetService("ReplicatedStorage").Events.__DFfDD:FireServer(unpack(args))
+end
+end)
+  Section:NewButton("Teleport to 2", "ButtonInfo", function()
+    shared.teleport = true
+local TPCFrame = CFrame.new(-26.4547081, 8.79999733, 225.65332, -0.499472409, 9.59097548e-08, 0.866329789, 4.83924865e-08, 1, -8.28080076e-08, -0.866329789, 5.63540048e-10, -0.499472409)--Put a new cframe or cframe property in here
+local User = game.Players.LocalPlayer.Character.HumanoidRootPart
+local WaitTime = 105 --Make More/Less depending if it works or not
+while shared.teleport do wait()
+spawn( function()
+wait(WaitTime)
+shared.teleport = false
+end)
+User.CFrame = TPCFrame
+local args = {
+    [1] = "__--r",
+    [2] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position,
+    [3] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0)
+}
+game:GetService("ReplicatedStorage").Events.__DFfDD:FireServer(unpack(args))
+end
+end)
+ Section:NewButton("Teleport to 3", "ButtonInfo", function()
+    shared.teleport = true
+local TPCFrame = CFrame.new(-64.3785172, 54.9999886, 501.533325, -0.786038458, 8.9924356e-08, 0.618177593, 9.67800133e-08, 1, -2.24070646e-08, -0.618177593, 4.22144204e-08, -0.786038458)--Put a new cframe or cframe property in here
+local User = game.Players.LocalPlayer.Character.HumanoidRootPart
+local WaitTime = 105 --Make More/Less depending if it works or not
+while shared.teleport do wait()
+spawn( function()
+wait(WaitTime)
+shared.teleport = false
+end)
+User.CFrame = TPCFrame
+local args = {
+    [1] = "__--r",
+    [2] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position,
+    [3] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0)
+}
+game:GetService("ReplicatedStorage").Events.__DFfDD:FireServer(unpack(args))
+end
+end)
+  Section:NewButton("Teleport to 4", "ButtonInfo", function()
+    shared.teleport = true
+local TPCFrame = CFrame.new(-181.985107, 56.7253418, 654.98291, -0.637351692, -7.69520625e-08, -0.77057308, -2.97179028e-08, 1, -7.52833316e-08, 0.77057308, -2.50821408e-08, -0.637351692)--Put a new cframe or cframe property in here
+local User = game.Players.LocalPlayer.Character.HumanoidRootPart
+local WaitTime = 105 --Make More/Less depending if it works or not
+while shared.teleport do wait()
+spawn( function()
+wait(WaitTime)
+shared.teleport = false
+end)
+User.CFrame = TPCFrame
+local args = {
+    [1] = "__--r",
+    [2] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position,
+    [3] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0)
+}
+game:GetService("ReplicatedStorage").Events.__DFfDD:FireServer(unpack(args))
+end
+end)
+ Section:NewButton("Teleport to 5", "ButtonInfo", function()
+    shared.teleport = true
+local TPCFrame = CFrame.new(-540.98999, 53.0625, 726.535645, 0.458663285, -5.21131824e-07, 0.888610125, 5.7831798e-08, 1, 5.56606892e-07, -0.888610125, -2.03905216e-07, 0.458663285)--Put a new cframe or cframe property in here
+local User = game.Players.LocalPlayer.Character.HumanoidRootPart
+local WaitTime = 105 --Make More/Less depending if it works or not
+while shared.teleport do wait()
+spawn( function()
+wait(WaitTime)
+shared.teleport = false
+end)
+User.CFrame = TPCFrame
+local args = {
+    [1] = "__--r",
+    [2] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position,
+    [3] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0)
+}
+game:GetService("ReplicatedStorage").Events.__DFfDD:FireServer(unpack(args))
+end
+end)
+ Section:NewButton("Teleport to 6", "ButtonInfo", function()
+    shared.teleport = true
+local TPCFrame = CFrame.new(-712.749207, 53.1491852, 745.219421, 0.608878732, -3.86413781e-08, 0.793263316, 1.35349483e-08, 1, 3.83230088e-08, -0.793263316, -1.25972868e-08, 0.608878732
+    )--Put a new cframe or cframe property in here
+local User = game.Players.LocalPlayer.Character.HumanoidRootPart
+local WaitTime = 105 --Make More/Less depending if it works or not
+while shared.teleport do wait()
+spawn( function()
+wait(WaitTime)
+shared.teleport = false
+end)
+User.CFrame = TPCFrame
+local args = {
+    [1] = "__--r",
+    [2] = game.Players.LocalPlayer.Character.HumanoidRootPart.Position,
+    [3] = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.Angles(0,0,0)
+}
+game:GetService("ReplicatedStorage").Events.__DFfDD:FireServer(unpack(args))
+end
+end)
